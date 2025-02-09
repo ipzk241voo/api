9e26902057b4fe890d463d6a7f8301779290d811
ndpower <alexeyvisnevskiy@gmail.com>
2025-02-09 22:12:58 +0200

    add config, product element to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { ApiService } from './api/api.service';
 import { ApiController } from './api/api.controller';
+
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './database/Product';
+
+import configuration from './config/configuration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
+
     MongooseModule.forRoot("mongodb://127.0.0.1:27017/test", {
       connectionName: "test",
       onConnectionCreate: (connection) => {
@@ -13,6 +23,10 @@ import { MongooseModule } from '@nestjs/mongoose';
         );
       }
     }),
+
+    MongooseModule.forFeature([
+      { name: "product", schema: ProductSchema }
+    ], "test"),
   ],
   controllers: [ApiController],
   providers: [ApiService],
