299cd743474bc658acbdc08cb6fc5cb95ff82ccf
ndpower <alexeyvisnevskiy@gmail.com>
2025-02-09 22:34:08 +0200

    add REST API request, (add, delete, update, ,get, getlist)

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiService } from './api.service';
 
 @Controller('api')
-export class ApiController {}
+export class ApiController {
+    constructor(
+        private readonly apiService: ApiService
+    ) { }
+
+    @Get([
+        "products",
+        "products/:productId"
+    ])
+    async products(
+        @Param("productId") productId: string,
+        @Body("selects") selects?: Array<string>,
+        @Query("limit") limit?: number,
+        @Query("page") page?: number
+    ) {
+        if (productId) {
+            return this.apiService.getProduct(productId)
+        }
+        return this.apiService.getProductList(limit, page, selects);
+    }
+
+    @Post("products")
+    async createProduct(
+        @Body("product") data: any
+    ) {
+        return this.apiService.createProduct(data);
+    }
+
+    @Patch("products/:productId")
+    async updateProduct(
+        @Param("productId") productId: string,
+        @Body() data: any
+    ) {
+        return this.apiService.updateProduct(productId, data);
+    }
+
+    @Delete("product/:productId")
+    async deleteProduct(
+        @Param("productId") productId: string
+    ) {
+        return this.deleteProduct(productId)
+    }
+}
--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,4 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { BadGatewayException, BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product } from 'src/database/Product';
+import { ProductType } from 'src/types/Product';
 
 @Injectable()
-export class ApiService {}
+export class ApiService {
+    constructor(
+        @InjectModel("product", "test") private product: Model<Product>
+    ) { }
+
+    async getProduct(_id: string) {
+        return this.product.findOne({ _id });
+    }
+
+    async getProductList(limit: number, page: number, selects?: Array<string>) {
+        return this.product.find({}, selects, { limit, skip: (page * limit) });
+    }
+
+    async createProduct(product: ProductType) {
+        const allowedKeys = Object.keys(this.product.schema.obj) as (keyof ProductType)[];
+        const extraKeys = Object.keys(product).filter(key => !allowedKeys.includes(key as keyof ProductType));
+
+        if (Object.keys(product).length == 0) {
+            throw new BadGatewayException("You have not specified any elements!", `Is not defined ${allowedKeys.toString()}!`)
+        }
+
+        if (extraKeys.length > 0) {
+            throw new BadRequestException(`The object contains invalid fields: ${extraKeys.join(', ')}`);
+        }
+
+        const created = new this.product(product);
+        const validate = created.validateSync();
+
+        if (validate?.errors) {
+            throw new BadRequestException(validate.message)
+        }
+
+        return created.save();
+    }
+
+    async updateProduct(
+        _id: string,
+        data: ProductType
+    ) {
+        const allowedKeys = Object.keys(this.product.schema.obj) as (keyof ProductType)[];
+
+        const keys = Object.keys(data)
+        const extraKeys = keys.filter(key => !allowedKeys.includes(key as keyof ProductType));
+
+        if (extraKeys.length > 0) {
+            throw new BadRequestException(`The object contains invalid fields: ${extraKeys.join(', ')}`);
+        }
+
+        const getElement = await this.product.findOne({ _id });
+        if (!getElement) {
+            throw new BadRequestException(`Nothing found for your id [${_id}]!`)
+        }
+
+        for (const key of keys) {
+            const validate = getElement.set(key, data[key]).validateSync();
+            if (validate?.errors) {
+                throw new BadGatewayException(validate.message)
+            }
+        }
+
+        return getElement.save();
+    }
+
+    async deleteProduct(
+        _id: string
+    ) {
+        return this.product.deleteOne({ _id });
+    }
+}
